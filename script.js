1d68f5bae648a122d5de9d6e2d597c06773d8cf7
0x00011 <59111912+0x00011@users.noreply.github.com>
2022-09-09 17:35:47 +0300

    Start rendering only after epilepsy warning

--- script.js
+++ script.js
@@ -16,6 +16,27 @@ function createPxElement(element) {
 }
 
 
+function startRendering() {
+	// Colorful Pixels
+	var width = document.body.clientWidth / 15;
+	var height = document.body.clientHeight / 15;
+	var i = 0;
+
+	while(i < height) {
+		var j = 0;
+		var row = document.createElement(`div`);
+		row.className = `row row-${i}`;
+		document.body.appendChild(row);
+
+		while(j < width) {
+			createPxElement(row)
+			j++;
+		}
+
+		i++;
+	}
+}
+
 
 // Epilepsy warning
 var timeRemaining = 5;
@@ -27,25 +48,5 @@ const timeInterval = setInterval(() => {
 setTimeout(() => {
 	document.querySelector('#warning').style.opacity = 0;
 	clearInterval(timeInterval);
+	startRendering();
 }, 5000);
-
-
-
-// Colorful Pixels
-var width = document.body.clientWidth / 15;
-var height = document.body.clientHeight / 15;
-var i = 0;
-
-while(i < height) {
-	var j = 0;
-	var row = document.createElement(`div`);
-	row.className = `row row-${i}`;
-	document.body.appendChild(row);
-
-	while(j < width) {
-		createPxElement(row)
-		j++;
-	}
-
-	i++;
-}
